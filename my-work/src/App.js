c3089250157d6749661c4793f1e151dd0018d5b9
visakh1042 <visakh1042@gmail.com>
2020-12-10 23:52:00 +0530

    event handler

--- my-work/src/App.js
+++ my-work/src/App.js
@@ -11,13 +11,32 @@ class App extends Component{
       { name: 'Stapanie', age: 28 },
     ]
   }
+  nameSwitchHandler = () =>{
+    console.log('clicked')
+  }
+  nameChangeHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Max', age: 28 },
+        { name: event.target.value, age: 29 },
+        { name: 'Stapanie', age: 28 },
+      ]
+    })
+  }
   render(){
     return (
       <div className="App">
-        <button>Switcher</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <button onClick={this.nameSwitchHandler }>Switcher</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age} />
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          changed={this.nameChangeHandler} />
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age} />
       </div>
     );
   }
--- my-work/src/Person/person.js
+++ my-work/src/Person/person.js
@@ -5,6 +5,7 @@ const person = (props) => {
     <div>
       <h1>{props.name} Hello world {props.age}</h1>
       <p>{props.children}</p>
+      <input type="text" onChange={props.changed} value={props.name}/>
     </div>
   )
 }
