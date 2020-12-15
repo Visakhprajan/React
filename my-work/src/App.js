0201887b3162ae34301f3a75f39272c640e0fe0c
Visakhprajan <visakhprajan@gmail.com>
2020-12-15 08:21:29 +0530

    Assignment start

--- my-work/src/App.js
+++ my-work/src/App.js
@@ -1,46 +1,18 @@
-import { Component } from 'react';
-import './App.scss';
-import Person from './Person/person';
+import './App.css';
+import UserInput from './UserInput/UserInput';
+import UserOutput from './UserOutput/UserOutput'
 
 
-class App extends Component{
-    state = {
-    persons: [
-      { name: 'Max', age: 28 },
-      { name: 'Manu', age: 29},
-      { name: 'Stapanie', age: 28 },
-    ]
-  }
-  nameSwitchHandler = () =>{
-    console.log('clicked')
-  }
-  nameChangeHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: 'Max', age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: 'Stapanie', age: 28 },
-      ]
-    })
-  }
-  render(){
-    return (
-      <div className="App">
-        <button onClick={this.nameSwitchHandler }>Switcher</button>
-        <Person 
-          name={this.state.persons[0].name} 
-          age={this.state.persons[0].age} />
-        <Person 
-          name={this.state.persons[1].name} 
-          age={this.state.persons[1].age}
-          changed={this.nameChangeHandler} />
-        <Person 
-          name={this.state.persons[2].name} 
-          age={this.state.persons[2].age} />
-      </div>
-    );
-  }
+
+function App() {
+  return (
+    <div className="App">
+      <UserInput />
+      <UserOutput />
+      <UserOutput />
+      <UserOutput />
+    </div>
+  );
 }
-
 
 export default App;
---
+++ my-work/src/UserOutput/UserOutput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const userOutput = () => {
+  return(
+    <div>
+      <p>Some text here</p>
+      <p>Some text here</p>
+    </div>
+  )
+}
+
+export default userOutput;
